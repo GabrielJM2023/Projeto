1568172d4f3615e97184c09a91e50f344f9d7f5a
GabrielJM2023 <gabrieljm@opcaovirtual.com.br>
2023-07-28 12:11:37 -0400

    Alterado o padrão de componentizaçao / Adicionado o click do botão / Alterado a estilização

---
+++ taperdido/src/components/Input/Input.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Input(props) {
+
+   return (
+      <input type={props}></input>
+   );
+}
+
+export default Input
--- taperdido/src/components/Input/Input_Senha/Input_Senha.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Input_Senha() {
-  return (
-    <input type={'password'}></input>
-  );
-}
-
-export default Input_Senha
