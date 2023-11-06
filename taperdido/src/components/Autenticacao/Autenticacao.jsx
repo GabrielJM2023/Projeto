7b5b57fe1d0d478cbe7b4c04c0eca75e849759e7
GabrielJM2023 <jardimgabriel2022@gmail.com>
2023-11-05 22:07:36 -0400

    Configuração de login
    Correção do erro que estava dando;

--- taperdido/src/components/Autenticacao/Autenticacao.jsx
+++ taperdido/src/components/Autenticacao/Autenticacao.jsx
@@ -1,9 +1,10 @@
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createClient } from '@supabase/supabase-js';
 
 export default function AuthForm() {
-   const supabase = createClientComponentClient()
+   const supabase = createClient('https://ryizcghnzfxeihrgjzzv.supabase.co',
+   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5aXpjZ2huemZ4ZWlocmdqenp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgwNjE4NDcsImV4cCI6MjAxMzYzNzg0N30.5o1U-cYQsMJogC2NE2kn0KJDvkf4KJYdmeX3VPJmHus')
 
    return (
       <Auth
