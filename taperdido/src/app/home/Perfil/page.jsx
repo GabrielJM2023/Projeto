ab68ac375c807b2b911b687b10a33bd852b3108d
GabrielJM2023 <gabrieljm@opcaovirtual.com.br>
2023-11-14 11:27:24 -0400

    Cofigurações do perfil

--- taperdido/src/app/home/Perfil/page.jsx
+++ taperdido/src/app/home/Perfil/page.jsx
@@ -1,11 +1,26 @@
 "use client"
 
 import React from 'React';
+import './estilo.css';
+import { RiEdit2Fill } from "react-icons/ri";
 
 export default function Perfil() {
     return (
         <>
-
+            <main className='Principal'>
+                <h1>Conta</h1>
+                <div>
+                    <label>Nome</label>
+                    <input></input>
+                    <label>E-mail</label>
+                    <input></input>
+                    <label>Senha</label>
+                </div>
+                <div>
+                    <label>Categorias Favoritas</label>
+                    <RiEdit2Fill />
+                </div>
+            </main>
         </>
     );
 }
