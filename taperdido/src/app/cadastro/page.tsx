14957484f49d1fb61dedf1c5315e5ebb129d0709
GabrielJM2023 <gabrieljm@opcaovirtual.com.br>
2023-11-24 13:43:38 -0400

    .

--- taperdido/src/app/cadastro/page.tsx
+++ taperdido/src/app/cadastro/page.tsx
@@ -1,8 +1,6 @@
 
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import { RedirectType } from 'next/navigation'
-import { redirect } from 'next/navigation'
 import Cadastro from './cadastro'
 
 export default async function PagePerfil() {
